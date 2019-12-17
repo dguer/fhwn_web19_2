b027b1536f53e205b42e5faa995e98b61078de4b
Dilber Gür <d.gur@gmx.de>
2019-12-17 19:13:16 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,48 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
-
-exports.default = defaultTask 
-
-'use strict';
-
 const gulp = require('gulp');
+const fs = require('fs');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
-
-sass.compiler = require('node-sass');
+const concat = require('gulp-concat');
 
 // Logs Message
-gulp.task('message', function(){
-  return console.log('Gulp is running...');
-});
-
-gulp.task('default', function(){
-  return console.log('Gulp is running...');
+gulp.task('message', function () {
+    return console.log('Gulp is running...');
 });
 
 // Copy HTML files
-gulp.task('copyHtml', function(){
-  gulp.src('src/*.html')
-    .pipe(gulp.dest('dist'));
+gulp.task('copyHtml', function () {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
 });
 
 // Minify JS
-gulp.task('minify', function(){
-  gulp.src('src/js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
+gulp.task('minify', async function () {
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
 });
 
 // Compile sass
-gulp.task('sass', function(){
-  gulp.src('src/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/styles'))
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('sass', async function () {
+    gulp.src('src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/styles'))
 });
 
 // Scripts
-gulp.task('scripts', function(){
-  gulp.src('src/js/*.js')
-    .pipe(concat())
-    .pipe(gulp.dest('dist/js'));
+gulp.task('scripts', async function () {
+    gulp.src('src/js/*.js')
+        .pipe(concat())
+        .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('default', ['message', 'copyHtml', 'copyHtml', 'minify','sass', 'scripts']);
+
+async function asyncAwaitTask() {
+    const { version } = fs.readFileSync('package.json');
+    console.log(version);
+    await Promise.resolve('some result');
+}
+
+exports.default = asyncAwaitTask;
+
+gulp.task('default', ['message', 'copyHtml', 'minify', 'sass', 'scripts']);

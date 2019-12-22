0fbf5879077a6b32335aff49c4bd63a49091551a
Dilber Gür <d.gur@gmx.de>
2019-12-22 23:03:25 +0100

    Init gh-pages

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const { src, dest, parallel, series, watch } = require('gulp');
+const concat = require('gulp-concat');
+const htmlhint = require("gulp-htmlhint");
+
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+var uncomment = require('gulp-uncomment');
+var favicons = require('gulp-favicons');
+
+const debug = require('gulp-debug');
+
+var deploy = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+async function deploy() {
+  return src("./dist/**/*")
+    .pipe(deploy())
+};
+
+function html() {
+  return src('src/*.html')
+	.pipe(debug({title: 'html:'}))
+	.pipe(htmlhint())
+	.pipe(htmlhint.failOnError())
+	.pipe(dest('dist'));
+}
+
+
+async function css() {
+  return src('src//sass/*.scss')
+	.pipe(debug({title: 'css :'}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist/css'));
+}
+
+async function js() {
+  return src('src/js/*.js', { sourcemaps: true })
+	.pipe(debug({title: 'js  :'}))
+    .pipe(concat('code.js'))
+	.pipe(uncomment({
+            removeEmptyLines: true
+        }))
+    .pipe(dest('dist/js', { sourcemaps: true }));
+}
+
+function ico() {
+    return src('src/*.ico')
+      .pipe(dest('dist'));
+  }
+
+exports.deploy= deploy;
+exports.js = js;
+exports.css = css;
+exports.html = series(html);
+exports.default = parallel(deploy, html, css, js, ico);
